a82dda9d08189d6bcbd9484b8f9d28cee5725023
caprice-j <skyfly0008@gmail.com>
2016-02-07 16:03:56 +0900

    roughly finished 3.AppIcon

---
+++ AppIcon/AppIcon/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AppIcon
+//
+//  Created by PCUser on 2/7/16.
+//  Copyright (c) 2016 PCUser. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
