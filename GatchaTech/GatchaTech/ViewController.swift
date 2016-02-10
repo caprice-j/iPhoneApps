3ae84493432907b1752a88da4b583f8b5bc6718e
caprice-j <skyfly0008@gmail.com>
2016-02-10 17:45:45 +0900

    roughly finished 7. Gatcha

---
+++ GatchaTech/GatchaTech/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  GatchaTech
+//
+//  Created by PCUser on 2/10/16.
+//  Copyright © 2016 PCUser. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func gacha() {
+        self.performSegueWithIdentifier("result", sender: nil)
+    }
+
+
+}
+
