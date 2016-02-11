0d78fd05c6996da10146f61f722f5236b9f86921
caprice-j <skyfly0008@gmail.com>
2016-02-11 14:22:13 +0900

    broken 9. WordList

---
+++ WordList/WordList/ListTableViewCell.swift
@@ -0,0 +1,27 @@
+//
+//  ListTableViewCell.swift
+//  WordList
+//
+//  Created by PCUser on 2/10/16.
+//  Copyright © 2016 PCUser. All rights reserved.
+//
+
+import UIKit
+
+class ListTableViewCell: UITableViewCell {
+    
+    @IBOutlet var englishLabel: UILabel!
+    @IBOutlet var japaneseLabel: UILabel!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ WordList/WordList/ViewController.swift
@@ -0,0 +1,52 @@
+//
+//  ViewController.swift
+//  WordList
+//
+//  Created by PCUser on 2/10/16.
+//  Copyright © 2016 PCUser. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func back(segue: UIStoryboardSegue){
+        
+    }
+    
+    @IBAction func startButtonTapped() {
+        let saveData = NSUserDefaults.standardUserDefaults()
+        
+        if let wordArray = saveData.arrayForKey("WORD") {
+            if wordArray.count > 0 {
+                self.performSegueWithIdentifier("toQuestionView", sender: nil)
+                return
+            }
+        }
+        
+        let alert: UIAlertController = UIAlertController(
+            title : "単語",
+            message: "まずは「単語一覧」をタップして単語登録して下さい",
+            preferredStyle: .Alert
+        )
+        
+        alert.addAction(UIAlertAction(
+            title: "OK",
+            style: .Default,
+            handler: nil
+            )
+        )
+        self.presentViewController(alert, animated: true, completion:nil)
+    }
+}
+
