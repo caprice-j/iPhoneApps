cbc713651145a7929433d5f02db52c6863b86502
caprice-j <skyfly0008@gmail.com>
2016-02-11 14:58:10 +0900

    roughly finished 10. BarabaraGame

---
+++ BarabaraGame/BarabaraGame/RankingViewController.swift
@@ -0,0 +1,48 @@
+//
+//  RankingViewController.swift
+//  BarabaraGame
+//
+//  Created by PCUser on 2/11/16.
+//  Copyright © 2016 PCUser. All rights reserved.
+//
+
+import UIKit
+
+class RankingViewController: UIViewController {
+
+    @IBOutlet var rankingLabel1: UILabel!
+    @IBOutlet var rankingLabel2: UILabel!
+    @IBOutlet var rankingLabel3: UILabel!
+    
+    let defaults: NSUserDefaults = NSUserDefaults.standardUserDefaults()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        rankingLabel1.text = String(defaults.integerForKey("score1"))
+        rankingLabel2.text = String(defaults.integerForKey("score2"))
+        rankingLabel3.text = String(defaults.integerForKey("score3"))
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func toTop() {
+        self.dismissViewControllerAnimated(true, completion: nil)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
