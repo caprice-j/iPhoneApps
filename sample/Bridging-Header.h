7cc468897d729b2d653f78f8a379c95783bb900a
caprice-j <skyfly0008@gmail.com>
2016-02-15 17:29:03 +0900

    add sample

---
+++ sample/Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  Bridging-Header.h
+//  sample
+//
+//  Created by PCUser on 2/13/16.
+//  Copyright © 2016 PCUser. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Bridging_Header : NSObject
+
+@end
---
+++ sample/Bridging-Header.m
@@ -0,0 +1,13 @@
+//
+//  Bridging-Header.m
+//  sample
+//
+//  Created by PCUser on 2/13/16.
+//  Copyright © 2016 PCUser. All rights reserved.
+//
+
+#import "Bridging-Header.h"
+
+@implementation Bridging_Header
+
+@end
